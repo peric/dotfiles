81a57268dc51df11a34c742be77fcf466a5360e3
zigomir <zigomir@gmail.com>
2013-06-26 13:30:19 +0200

    adding Astina stuff

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/Cellar/php54/5.4.15/bin:$HOME/.astina-dotfiles:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
