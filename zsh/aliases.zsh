a36c7106188e70ec5a67ba0317a6fda49fc0bdcb
zigomir <zigomir@gmail.com>
2013-12-18 21:24:42 +0100

    adding elastic search run alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,9 @@
 alias reload!='. ~/.zshrc'
+
+# Vagrant
 alias vs='vagrant ssh'
 alias vsus='vagrant suspend'
 alias vu='vagrant up'
+
+# ElasticSearch
+alias elas='elasticsearch -f -D es.config=/usr/local/opt/elasticsearch/config/elasticsearch.yml'
