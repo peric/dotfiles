95543f85c839f37c956d32c9a511344ffb5a7d67
zigomir <zigomir@gmail.com>
2013-12-30 14:16:16 +0100

    pimpin mah dotfilez

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,6 @@ alias vu='vagrant up'
 
 # ElasticSearch
 alias elas='elasticsearch -f -D es.config=/usr/local/opt/elasticsearch/config/elasticsearch.yml'
+
+# Postgres
+alias pg='postgres -D /usr/local/var/postgres'
